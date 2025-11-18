27a8b0a979c05df07bda323f7d310c9a54c7d7a5
GuillermoInfiesta <ginfiestap@gmail.com>
2025-11-18 23:32:35 +0100

    Initial commit with diff functionality
    In this first commit the tool is capable of running a west diff
    if run inside a .venv or other enviromen which has west installed

---
+++ west_gui.py
@@ -0,0 +1,78 @@
+import shutil
+import subprocess
+import sys
+import tkinter as tk
+from tkinter import ttk
+import threading
+
+west_path=None
+diff_button=None
+logging_box=None
+logging_box_ocupation=0
+_NEW_LINE_ = "\r\n"
+_MAX_LOG_LINES=500
+
+def display_log(line):
+	global logging_box, logging_box_ocupation
+	if(logging_box_ocupation + 1 > _MAX_LOG_LINES):
+		logging_box.delete("1.0", "2.0")
+		logging_box_ocupation = logging_box_ocupation - 1
+
+	logging_box.insert(tk.END, line)
+	logging_box_ocupation = logging_box_ocupation + 1
+	logging_box.see(tk.END)
+	logging_box.update_idletasks()
+
+def run_west_command(command):
+	global west_path, diff_button, logging_box
+	display_log(f"-> {west_path} {command}" + _NEW_LINE_)
+	pc = subprocess.Popen(f"{west_path} {command}",
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            text=True,
+            encoding='utf-8',
+			shell=True)
+
+	for line in pc.stdout:
+		display_log(line)
+	pc.wait()
+
+	return pc.stdout, pc.returncode
+
+def west_diff():
+	global diff_button
+	diff_button.config(state=tk.DISABLED, text="Running diff")
+	logs, rc = run_west_command("diff")
+	diff_button.config(state=tk.NORMAL, text="Run diff")
+
+def on_diff_button():
+	threading.Thread(target=west_diff).start()
+
+def main():
+	global west_path, diff_button, logging_box
+	west_path = shutil.which('west')
+	if west_path:
+		print(f"West found at {west_path}")
+	else:
+		print("West not found")
+
+	root = tk.Tk()
+	root.title("West GUI Tool")
+	root.geometry("1080x720")
+
+	diff_button = tk.Button(root, text="Run diff", command=on_diff_button)
+	diff_button.pack(padx=20, pady=20)
+
+	frame = ttk.Frame(root)
+	frame.pack(padx=20, pady=10, side="bottom", fill="x")
+	logging_box = tk.Text(frame, state="normal", wrap="word", height=10, width=680)
+	scroll = ttk.Scrollbar(frame, orient="vertical", command=logging_box.yview)
+	logging_box.config(yscrollcommand=scroll.set)
+	logging_box.pack(side="left")
+	scroll.pack(side="right")
+	
+	display_log("Logs from west commands will show here" + _NEW_LINE_)
+	root.mainloop()
+
+if __name__ == "__main__":
+	main()
