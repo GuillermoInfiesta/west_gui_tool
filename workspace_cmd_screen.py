c8ffec803def6066f47e8de0babf7cd749326b81
GuillermoInfiesta <ginfiestap@gmail.com>
2025-11-22 21:31:25 +0100

    Add workspace clean button

--- workspace_cmd_screen.py
+++ workspace_cmd_screen.py
@@ -7,6 +7,7 @@ class WorkspaceCmdFrame(tk.Tk):
         self.main_frame = tk.Frame(root)
         self.cmd_cb = cmd_cb
         self.diff_btn = tk.Button(self.main_frame, text="Run diff")
+        self.clean_btn = tk.Button(self.main_frame, text="Clean workspace")
     
     def create_window(self, x, y, height, width):
         self.main_frame.config(height=height, width=width)
@@ -22,8 +23,16 @@ class WorkspaceCmdFrame(tk.Tk):
         self.diff_btn.pack(padx=20, pady=20)
 
         #Clean workspace
+        def clean_btn_cb():
+            self.__config_buttons_state(state=tk.DISABLED)
+            self.cmd_cb("forall -c \"git restore . && git clean -fdx\"")
+            self.__config_buttons_state(state=tk.ACTIVE)
+
+        self.clean_btn.config(command=lambda: threading.Thread(target=clean_btn_cb).start())
+        self.clean_btn.pack(padx=100, pady=20)
         #Change active project
         #Update
     
     def __config_buttons_state(self, state):
         self.diff_btn.config(state=state)
+        self.clean_btn.config(state=state)
