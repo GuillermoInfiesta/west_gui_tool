c3977b8aa7b1638d02f4aff6d282b76f6d00e511
GuillermoInfiesta <ginfiestap@gmail.com>
2025-11-24 21:33:39 +0100

    View and select manifest path target

--- workspace_cmd_screen.py
+++ workspace_cmd_screen.py
@@ -1,5 +1,26 @@
 import tkinter as tk
 import threading
+import os
+from tkinter import filedialog
+
+def get_workspace_manifest_path():
+	west_config_path = os.path.join(os.getcwd(), ".west", "config")
+	west_config = open(west_config_path)
+	west_config.readline() #Discard first line
+	manifest_path = west_config.readline().split("=")[1].strip()
+	west_config.close()
+	return manifest_path
+
+def set_workspace_manifest_path(relative_path):
+    west_config_path = os.path.join(os.getcwd(), ".west", "config")
+    west_config = open(west_config_path, "r+")
+    lines = west_config.readlines()
+    lines[1] = f"path = {relative_path}\n"
+    west_config.seek(0)
+    west_config.writelines(lines)
+    west_config.truncate()
+    west_config.close()
+
 
 class WorkspaceCmdFrame(tk.Tk):
     def __init__(self, root, cmd_cb):
@@ -8,6 +29,8 @@ class WorkspaceCmdFrame(tk.Tk):
         self.cmd_cb = cmd_cb
         self.diff_btn = tk.Button(self.main_frame, text="Run diff")
         self.clean_btn = tk.Button(self.main_frame, text="Clean workspace")
+        self.manifest_path = tk.Frame(self.main_frame)
+        self.manifest_path_string = tk.StringVar(value=get_workspace_manifest_path())
     
     def create_window(self, x, y, height, width):
         self.main_frame.config(height=height, width=width)
@@ -30,7 +53,25 @@ class WorkspaceCmdFrame(tk.Tk):
 
         self.clean_btn.config(command=lambda: threading.Thread(target=clean_btn_cb).start())
         self.clean_btn.pack(padx=100, pady=20)
+
         #Change active project
+        self.manifest_path.place(x=0, y=200, height= 100, width=400)
+        manifest_path_text = tk.Entry(self.manifest_path, width=300,
+                                              state="readonly", textvariable=self.manifest_path_string)
+        def manifest_path_select():
+            manifest_path_dir = filedialog.askdirectory(mustexist=True)
+            manifest_path_dir = os.path.normpath(manifest_path_dir)
+
+            if(os.path.exists(os.path.join(manifest_path_dir, "west.yml")) and
+               manifest_path_dir.startswith(os.getcwd())):
+                relative_path = (os.path.relpath(manifest_path_dir, os.getcwd()))
+                set_workspace_manifest_path(relative_path)
+                self.manifest_path_string.set(get_workspace_manifest_path())
+
+        manifest_path_button = tk.Button(self.manifest_path, text="Change manifest path",
+                                         command=manifest_path_select)
+        manifest_path_button.pack(side="left")
+        manifest_path_text.pack(side="right")
         #Update
     
     def __config_buttons_state(self, state):
